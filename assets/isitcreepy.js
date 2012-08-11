605aba3c639a6e8be2d119a2d96d6abc55030d49
José de Paula Eufrásio Júnior <jose.junior@gmail.com>
2012-08-11 11:59:32 -0300

    Hardcoded localhost ftl

--- assets/isitcreepy.js
+++ assets/isitcreepy.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#age_selector").change(function() {
       var value = $("#age_selector").val()
-      $.getJSON('http://localhost:9080/calc/' + value, function(data) {
+      $.getJSON('/calc/' + value, function(data) {
         var inner = "<p id=phrase>The youngest person you should date is <strong>" + data["Min"] +
         "</strong> years old, and the oldest person that should date you is <strong>" + data["Max"] +
         "</strong> years old.</p>"
